47b080c4034bbb7b380d231013e4f8bb8c5ef6de
Simon Sturgess <dahliacreative@users.noreply.github.com>
2020-11-05 15:26:28 +0000

    Background attachment

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const Wrapper = styled.section`
   padding: 4em 0 5em;
+  background-attachment: fixed;
   ${({ dark, light }) => {
     if (dark) {
       return `
